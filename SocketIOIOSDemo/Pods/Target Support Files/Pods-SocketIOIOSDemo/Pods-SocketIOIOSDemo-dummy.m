188a3a2dfcc7d9d66ea778d5fb43dd27734b2c13
wangnamu <wangstin@sina.com>
2017-01-11 21:07:10 +0800

    create & upload

---
+++ SocketIOIOSDemo/Pods/Target Support Files/Pods-SocketIOIOSDemo/Pods-SocketIOIOSDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocketIOIOSDemo : NSObject
+@end
+@implementation PodsDummy_Pods_SocketIOIOSDemo
+@end
---
+++ SocketIOIOSDemo/SocketIOIOSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocketIOIOSDemo
+//
+//  Created by tjpld on 2017/1/11.
+//  Copyright © 2017年 ufo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
