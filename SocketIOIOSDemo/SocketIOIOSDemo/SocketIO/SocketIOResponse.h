7b1dfd2499694f757b54e735caa5121b3ae10dcc
wangnamu <wangstin@sina.com>
2017-07-21 16:00:45 +0800

    fix kickoff bug

---
+++ SocketIOIOSDemo/SocketIOIOSDemo/SocketIO/SocketIOResponse.h
@@ -0,0 +1,13 @@
+//
+//  SocketIOResponse.h
+//  SocketIOIOSDemo
+//
+//  Created by tjpld on 2017/7/19.
+//  Copyright © 2017年 ufo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketIOResponse : NSObject
+
+@end
---
+++ SocketIOIOSDemo/SocketIOIOSDemo/SocketIO/SocketIOResponse.m
@@ -0,0 +1,13 @@
+//
+//  SocketIOResponse.m
+//  SocketIOIOSDemo
+//
+//  Created by tjpld on 2017/7/19.
+//  Copyright © 2017年 ufo. All rights reserved.
+//
+
+#import "SocketIOResponse.h"
+
+@implementation SocketIOResponse
+
+@end
